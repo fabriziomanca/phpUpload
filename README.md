92ba4efada1c9b0e480856c7415a2f2d15d24f3d
Fabrizio Manca <biziom87@gmail.com>
2020-10-07 06:46:30 +0200

    Beta Version

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# phpUpload
+# Php Upload Manager
+
+[Php Upload Repository][repolink]
+
+Manage your upload as easiest as possible!
+
+## Installation
+<br/>
+
+Via [Composer][dc1]:
+
+    $ composer require fabriziomanca/phpupload
+
+<br/>
+
+## Usage
+<br/>
+
+#### Basic usage
+```php
+use fabriziomanca\phpupload\phpUpload;
+$pUp = new phpUpload($_FILES['file']);
+```
+
+#### Advanced usage
+```php
+use fabriziomanca\phpupload\phpUpload;
+$pUp = new phpUpload($_FILES['file']);
+$pUp->maxSize('1024'); // Set a Size Limit in KB
+$pUp->allowedType(["image/jpeg", "image/png"]); // Set allowed mime type (image/jpeg, application/pdf, image/png, etc)
+$pUp->newName("New_name"); // Set a new name for the file
+$pUp->run('destination/folder'); // Move the file to a specific folder
+```
+
+
+<br/>
+
+[dc1]: http://getcomposer.org/
+[repolink]: https://github.com/fabriziomanca/phpUpload
--- src/phpUpload.php
+++ src/phpUpload.php
@@ -2,11 +2,63 @@
 
 namespace fabriziomanca\phpUpload;
 
+use Exception;
+
 class phpUpload{
+
+    private $upFile;
+    public  $Name;
+    public  $FileName;
+    public  $extension;
+    public  $fileSize;
 
     public function __construct($file){
 
-        return $file;
+        $this->upFile = $file;
+
+        $this->Name = $this->upFile['name'];
+
+        $this->extension = substr($this->Name, strpos($this->Name, '.')+1);
+
+        $this->FileName = substr($this->Name, 0, strpos($this->Name, '.'));
+
+        $this->fileSize = number_format($this->upFile['size']/1024, 2); //size in KB
+
+    }
+
+    public function maxSize($maxs){
+
+        if($this->fileSize > $maxs){
+
+            throw new Exception("Error with the File Size! Max Allowed = $maxs KB");
+
+        }
+
+    }
+
+    public function allowedType($allExt){
+
+        if(!in_array($this->upFile['type'], $allExt)){
+
+            throw new Exception("Mime type not allowed! Allowed = " . implode(', ', $allExt));
+
+        } 
+
+    }
+
+    public function newName($newName){
+
+        $this->FileName = $newName;
+
+    }
+
+    public function run($destination){
+
+        if(!move_uploaded_file($this->upFile['tmp_name'], $destination . "/" . $this->FileName . "." . $this->extension)){
+
+            throw new Exception("Error with the moving!");
+
+        }
 
     }
 
